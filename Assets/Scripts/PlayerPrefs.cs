3b84bc22048b144c34fda0b31a36f00313662586
geia <111124706+geheyah@users.noreply.github.com>
2023-03-18 14:58:23 +0800

    ui

---
+++ Assets/Scripts/PlayerPrefs.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefs : MonoBehaviour
+{
+    // TASK: To make a Save and Load script, F2 to Save and F3 to load, using KeyCodes
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
